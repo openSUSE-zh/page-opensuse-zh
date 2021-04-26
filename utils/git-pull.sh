7bb3391491d1a4f3f859f58f495c5fe4f87afe5f
ImBearChild <imbearchild@outlook.com>
2021-04-26 20:42:38 +0800

    更正文件位置

--- utils/git-pull.sh
+++ utils/git-pull.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 date
 git --git-dir=/home/bearchild/html-opensuse-zh/.git pull --force
+git --git-dir=/home/bearchild/html-opensuse-zh/.git clean -fd 
 git --git-dir=/home/bearchild/page-opensuse-zh/.git pull --force
+git --git-dir=/home/bearchild/html-opensuse-zh/.git clean -fd 
 rsync -av --exclude .git/ /home/bearchild/html-opensuse-zh/ /srv/www/vhosts/suse.org.cn
