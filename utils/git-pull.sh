daa1731acc836d0886ba7f926add28a941be7c41
ImBearChild <imbearchild@outlook.com>
2021-04-26 19:50:30 +0800

    更新网页更新脚本

--- utils/git-pull.sh
+++ utils/git-pull.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-cd page-opensuse-zh
-git pull --force
+
+git --git-dir=/home/bearchild/html-opensuse-zh/ pull --force
+git --git-dir=/home/bearchild/page-opensuse-zh/ pull --force
+rsync -av --exclude .git/ /home/bearchild/html-opensuse-zh/ /srv/www/vhosts/suse.org.cn
