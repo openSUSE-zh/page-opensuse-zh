a4dbe3252f11e00b35cffe9633569f5342dc042e
ImBearChild <imbearchild@outlook.com>
2021-04-26 21:07:58 +0800

    修正脚本

--- utils/git-pull.sh
+++ utils/git-pull.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
 date
-git --git-dir=/home/bearchild/html-opensuse-zh/.git pull --force
+git --git-dir=/home/bearchild/html-opensuse-zh/.git --work-tree=/home/bearchild/html-opensuse-zh/ pull --branch=gh-pages --force
+git --git-dir=/home/bearchild/html-opensuse-zh/.git --work-tree=/home/bearchild/html-opensuse-zh/ checkout gh-pages --force
 git --git-dir=/home/bearchild/html-opensuse-zh/.git --work-tree=/home/bearchild/html-opensuse-zh/ clean -fd 
-git --git-dir=/home/bearchild/page-opensuse-zh/.git pull --force
+git --git-dir=/home/bearchild/page-opensuse-zh/.git --work-tree=/home/bearchild/page-opensuse-zh/ pull --force
+git --git-dir=/home/bearchild/page-opensuse-zh/.git --work-tree=/home/bearchild/page-opensuse-zh/ checkout main --force
 git --git-dir=/home/bearchild/page-opensuse-zh/.git --work-tree=/home/bearchild/page-opensuse-zh/ clean -fd 
 rsync -av --exclude .git/ /home/bearchild/html-opensuse-zh/ /srv/www/vhosts/suse.org.cn
